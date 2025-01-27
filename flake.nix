38f181cc8053f0843984269d5a808b64d41c5416
Nick Girardo <nickgirardo@gmail.com>
2025-01-26 19:00:38 -0500

    GTE Version bump

--- flake.nix
+++ flake.nix
@@ -20,11 +20,11 @@
     in
     {
       packages = forAllSystems ({ system, pkgs, ... }: let
-        gte_rev = "6c368f0e553ffbc93b3717cf83c62c68277c15d5";
+        gte_rev = "619d790bc815582805c3e61ee04e1a5cf69fa728";
         src = pkgs.fetchgit {
           url = "https://github.com/clydeshaffer/GameTankEmulator.git";
           rev = gte_rev;
-          sha256 = "sha256-IQqntQbS8KC1cwokYHPNmPbTo+B+3jhRK/ghmxiK5tE=";
+          sha256 = "sha256-Fvfi0EI2IxDEipe6x9sEwwf1cI4uWp0UKBfkVx2iED8=";
           fetchSubmodules = true;
         };
         SDL2_rev = "release-2.28.4";
